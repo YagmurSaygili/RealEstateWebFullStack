1baf5073492f28ef665efce4ce6f3b3bfd0dea52
YagmurSaygili <yagmur.saygili@windowslive.com>
2024-02-13 14:30:48 +0300

    Empty Commit With Search Bar Page, For Backup

---
+++ server/routes/SearchBar.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+// const { Comments } = require("../models");
+const { validateToken } = require("../middlewares/AuthMiddleware");
+
+
+
+module.exports = router;
